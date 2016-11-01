e78e1cbc57e377e9e8b6ccdc6c0c58663aa97693
gardners <paul@servalproject.org>
2016-11-01 16:22:51 +0100

    work on getting DMA operations to actually work

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 
 int main()
 {
-  printf("Hello world.\n");
+  // Enable C65 features
+  POKE(0xd02fU,0xA5);
+  POKE(0xd02fU,0x96);
+
+  //  POKE(0xd031U,0xff);
+  
+  POKE(0x0426,0);
+  
+  lpoke(0x0427,0x22);
+  
+  //  printf("$0400 = $%02x\n",lpeek(0x0400));
   return 0;
 }
--- memory.c
+++ memory.c
@@ -5,14 +5,72 @@
 
 */
 
+#include "memory.h"
+
+struct dmagic_dmalist {
+  unsigned char command;
+  unsigned int count;
+  unsigned int source_addr;
+  unsigned char source_bank;
+  unsigned int dest_addr;
+  unsigned char dest_bank;
+  unsigned int modulo;
+};
+
+struct dmagic_dmalist dmalist;
+unsigned char dma_byte;
+
 unsigned char lpeek(long address)
 {
   // Read the byte at <address> in 28-bit address space
-  return 0xff;
+  // XXX - Optimise out repeated setup etc
+  // (separate DMA lists for peek, poke and copy should
+  // save space, since most fields can stay initialised).
+  dmalist.command=0x00; // copy
+  dmalist.count=1;
+  dmalist.source_addr=address&0xffff;
+  dmalist.source_bank=(address>>16)&0x7f;
+  dmalist.dest_addr=(unsigned int)&dma_byte;
+  dmalist.dest_bank=0;
+
+  // Now run DMA job (to and from low 1MB, and list is in low 1MB)
+  POKE(0xd702U,0);
+  POKE(0xd704U,0);
+  POKE(0xd705U,0);
+  POKE(0xd706U,0);
+  POKE(0xd701U,((unsigned int)&dmalist)>>8);
+  POKE(0xd700U,((unsigned int)dmalist)&0xff); // triggers DMA
+  
+  return dma_byte;
 }
 
 void lpoke(long address, unsigned char value)
 {
+  unsigned char i;
+  unsigned char v;
+  
+  dma_byte=value;
+  dmalist.command=0x00; // copy
+  dmalist.count=1;
+  dmalist.dest_addr=address&0xffff;
+  dmalist.dest_bank=(address>>16)&0x7f;
+  dmalist.source_addr=(unsigned int)&dma_byte;
+  dmalist.source_bank=0;
+
+  for(i=0;i<12;i++) {
+    v=PEEK((unsigned int)(&dmalist)+i);
+    POKE(0x0400U+i,v);
+  }
+  POKE(0x0428U,(unsigned int)&dma_byte&0xff);
+  POKE(0x0429U,(((unsigned int)&dma_byte)>>8)&0xff);
+  
+  // Now run DMA job (to and from low 1MB, and list is in low 1MB)
+  POKE(0xd702U,0);
+  POKE(0xd704U,0);
+  POKE(0xd705U,0);
+  POKE(0xd706U,0);
+  POKE(0xd701U,((unsigned int)&dmalist)>>8);
+  POKE(0xd700U,((unsigned int)dmalist)&0xff); // triggers DMA
   return;
 }
 

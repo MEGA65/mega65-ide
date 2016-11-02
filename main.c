22b93c7bde316cbeb1b4001dd9c941005e780ce9
gardners <paul@servalproject.org>
2016-11-02 13:50:48 +0100

    remove debugging and opt65 work-around. initialise screen on start

--- main.c
+++ main.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 #include "memory.h"
-
-void write_d02f(unsigned char v)
-{
-  POKE(0xd02fU,v);
-}
+#include "screen.h"
 
 int main()
 {
   // Enable C65 features
-  write_d02f(0xA5);
-  write_d02f(0x96);
-
-  //  POKE(0xd031U,0xff);
+  POKE(0xd02fU,0xA5);
+  POKE(0xd02fU,0x96);
   
-  POKE(0x0426,0);
+  setup_screen();
   
-  lpoke(0x0427,0x22);
-
-  POKE(0x0425,0);
-
-  //  printf("$0400 = $%02x\n",lpeek(0x0400));
   return 0;
 }

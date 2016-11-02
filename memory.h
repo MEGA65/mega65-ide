f79efaa0fed0809caddefd4da4ccfd983ca50c6e
gardners <paul@servalproject.org>
2016-11-02 13:51:18 +0100

    update memory map. begin implementing screen functions.

--- memory.h
+++ memory.h
@@ -1,3 +1,24 @@
+/*
+  Overall Memory layout:
+
+  $00000-$003FF - C64 ZP, stack etc
+  $00400-$007FF -                               1KB UNUSED
+  $00800-$09FFF - M65IDE program
+  $0A000-$0B7FF -                               6KB unused (CC65 banks our BASIC)
+  $0B800-$0BFFF - 2KB screen RAM
+  $0C000-$0FFFF -                               16KB unused (some under IO/ROM)
+  $10000-$11FFF - C65 DOS
+  $12000-$1F7FF - 54KB RAM for buffers etc
+  $1F800-$1FFFF - C65 2KB colour RAM for screen
+
+  Screen is the main thing to sort out, because we need to have the C64/C65 character
+  set in scope for the VIC-III (we are not using MEGA65-only VIC-IV arbitrary screen
+  buffer registers).  This requires 2KB somewhere.  Ideally we don't want to mess up
+  the contiguous 54KB of RAM in the 2nd 64KB bank, that we will use for buffers for
+  files being edited.
+
+*/
+
 unsigned char lpeek(long address);
 void lpoke(long address, unsigned char value);
 void lcopy(long source_address, long destination_address,
---
+++ screen.c
@@ -0,0 +1,70 @@
+#include "memory.h"
+
+#define FOOTER_COPYRIGHT 0
+#define FOOTER_BLANK 1
+#define FOOTER_MAX 1
+unsigned char *footer_messages[2]={
+  "MEGA65 IDE v00.01 : (C) Copyright 2016 Paul Gardner-Stephen etc.  CTRL-q to exit",
+  "                                                                                "
+};
+unsigned char footers_initialised=0;
+
+unsigned char ascii_to_screen(unsigned char in)
+{
+  if ((in>='A')&&(in<='Z')) return (in-0x00);
+  if ((in>='a')&&(in<='z')) return (in-0x40);
+  return in;
+}
+
+void initialise_footers(void)
+{
+  unsigned char i,f;
+  for(f=0;f<=FOOTER_MAX;f++) {
+    unsigned char *p=footer_messages[f];
+    for(i=0;i<80;i++)
+      p[i]=ascii_to_screen(p[i])|0x80;
+  }
+}
+
+void display_footer(unsigned char index)
+{  
+  unsigned char i;
+
+  if (!footers_initialised) initialise_footers();
+  
+  lcopy(footer_messages[index],(0xb800U+24*80),80);
+  for(i=0;i<80;i++)
+    POKE((0xb800U+24*80)+i,PEEK((0xb800U+24*80)+i)|0x80);
+}
+
+void setup_screen(void)
+{
+  unsigned char v;
+  
+  // 80-column mode, fast CPU, extended attributes enable
+  *((unsigned char*)0xD031)=0xe0;
+
+  // Put screen memory somewhere (2KB required)
+  // We are using $B800-$BFFF for now
+  // and use lower-case char set
+  *(unsigned char *)0xD018U=0xE6;
+
+  // VIC RAM Bank to $C000-$FFFF
+  v=*(unsigned char *)0xDD00U;
+  v&=0xfc;
+  v|=0x01;
+  *(unsigned char *)0xDD00U=v;
+
+  // Screen colours
+  POKE(0xD020U,0);
+  POKE(0xD021U,6);
+
+  // Clear screen RAM
+  lfill(0x0b800,0x20,2000);
+
+  // Clear colour RAM: white text
+  lfill(0x1f800,0x01,2000);
+
+  display_footer(FOOTER_COPYRIGHT);
+  
+}

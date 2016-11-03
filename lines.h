b9e8cc54aaf19609f07a7001495a4470178baaf3
gardners <paul@servalproject.org>
2016-11-03 15:38:28 +0100

    export line buffer via header

--- lines.h
+++ lines.h
@@ -1,1 +1,10 @@
+extern unsigned char line_buffer_buffer_id;
+extern unsigned int line_buffer_line_number;
+extern unsigned char line_buffer[255];
+extern unsigned char line_buffer_length;
+extern unsigned char line_buffer_original_length;
+extern unsigned char line_buffer_dirty;
+
+
 unsigned int line_find_offset(unsigned char buffer_id, unsigned int line_number);
+unsigned char line_fetch(unsigned char buffer_id, unsigned int line_number);
